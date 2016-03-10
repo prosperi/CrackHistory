c3c80bbf89b871d35b65ba62a0a5a33b35a3ad6c
Zura <prosperrriii@gmail.com>
2016-03-11 01:07:16 +0400

    Started working on question view

--- crackHistoryApp/www/js/questions/controllers.js
+++ crackHistoryApp/www/js/questions/controllers.js
@@ -12,12 +12,21 @@ angular.module('questions', ['questionsSrv'])
   var questions = JSON.parse(window.localStorage.getItem("questionList"));
 
   $scope.question = questions[0];
+  $scope.choice = "here";
+  $scope.turn = false;
 
   $scope.show = function(){
     $ionicLoading.show({
         template: '<ion-spinner icon="ripple" class="spinner-positive"></ion-spinner>',
         duration: 500
       });
+    console.log($scope.choice);
+    if($scope.choice != $scope.question.correct_answer){
+      console.log("You are mistaken my dear!");
+    }else{
+      console.log("Good job man, I'm proud of you!");
+    }
+    $scope.turn = true;
   };
 
 }])
--- crackHistoryApp/www/js/questions/services.js
+++ crackHistoryApp/www/js/questions/services.js
@@ -4,19 +4,31 @@ angular.module('questionsSrv', [])
 
   var categories = {};
 
-  categories.categoryList = function(){
-    return $http({
+  $http({
       method: "GET",
       url: "https://notgiorgi.com/api/category/"
-    });
-  };
+    }).then(function(response){
+    categories.categoryList = [];
+    for(key in response.data){
+      categories.categoryList.unshift(response.data[key]);
+    }
+    window.localStorage.setItem("categoryList", JSON.stringify(categories.categoryList));
+  }, function(response){
+    console.log("Fetching categories failed");
+  });
 
-  categories.questionList = function(){
-    return $http({
+  $http({
       method: "GET",
       url: "https://notgiorgi.com/api/test/"
-    });
-  };
+    }).then(function(response){
+    categories.questionList = [];
+    for(key in response.data){
+      categories.questionList.unshift(response.data[key]);
+    }
+    window.localStorage.setItem("questionList", JSON.stringify(categories.questionList));
+  }, function(response){
+    console.log("Fetching categories failed");
+  });
 
   return categories;
 
