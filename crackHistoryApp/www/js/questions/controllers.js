4e2b95a59506d0bc82bfbb9e23bb4e059e5feef4
Zura <prosperrriii@gmail.com>
2016-03-12 23:54:05 +0400

    Fixed the question part

--- crackHistoryApp/www/js/questions/controllers.js
+++ crackHistoryApp/www/js/questions/controllers.js
@@ -6,7 +6,7 @@ angular.module('questions', ['questionsSrv'])
 
 }])
 
-.controller('questionsCtrl', ['$scope', '$stateParams', '$ionicLoading', '$timeout', '$state', function($scope, $stateParams, $ionicLoading, $timeout, $state){
+.controller('questionsCtrl', ['$scope', '$stateParams', '$ionicLoading', '$timeout', '$state', '$ionicPopup', function($scope, $stateParams, $ionicLoading, $timeout, $state, $ionicPopup){
 
   $scope.category = JSON.parse(window.localStorage.getItem("categoryList"))[$stateParams.id];
   var questions = JSON.parse(window.localStorage.getItem("questionList"));
@@ -17,48 +17,59 @@ angular.module('questions', ['questionsSrv'])
   $scope.choice = "";
   $scope.turn = false;
 
-  $scope.show = function(event){
-    $ionicLoading.show({
-        template: '<ion-spinner icon="ripple" class="spinner-positive" ></ion-spinner>',
-        animation: 'fade-in'
-      });
+  $scope.check = function(){
 
-
-    $timeout(function () {
+      var selected = document.getElementsByClassName("radio" + $scope.choice)[$scope.currentQuestion].childNodes[0];
 
       if($scope.choice != $scope.question.correct_answer){
 
-        event.target.parentElement.parentElement.childNodes[$scope.question.correct_answer*2].style.borderColor = "#33CD5F";
-        event.target.parentElement.parentElement.childNodes[$scope.question.correct_answer*2].childNodes[1].childNodes[0].style.backgroundColor = "#33CD5F";
-        event.target.parentElement.parentElement.childNodes[$scope.question.correct_answer*2].childNodes[1].childNodes[1].className = "radio-icon disable-pointer-events icon ion-checkmark";
-        event.target.parentElement.parentElement.childNodes[$scope.question.correct_answer*2].childNodes[1].childNodes[1].style.visibility = "visible";
+        selected.parentElement.parentElement.childNodes[$scope.question.correct_answer*2].style.borderColor = "#33CD5F";
+        selected.parentElement.parentElement.childNodes[$scope.question.correct_answer*2].childNodes[1].childNodes[0].style.backgroundColor = "#33CD5F";
+        selected.parentElement.parentElement.childNodes[$scope.question.correct_answer*2].childNodes[1].childNodes[1].className = "radio-icon disable-pointer-events icon ion-checkmark";
+        selected.parentElement.parentElement.childNodes[$scope.question.correct_answer*2].childNodes[1].childNodes[1].style.visibility = "visible";
 
-        event.target.parentElement.style.borderColor = "#EF473A";
-        event.target.parentElement.childNodes[1].childNodes[0].style.backgroundColor = "#EF473A";
-        event.target.parentElement.childNodes[1].childNodes[1].className = "radio-icon disable-pointer-events icon ion-close";
+        selected.parentElement.style.borderColor = "#EF473A";
+        selected.parentElement.childNodes[1].childNodes[0].style.backgroundColor = "#EF473A";
+        selected.parentElement.childNodes[1].childNodes[1].className = "radio-icon disable-pointer-events icon ion-close";
         console.log("You are mistaken my dear!");
 
       }else{
-        event.target.parentElement.style.borderColor = "#33CD5F";
-        event.target.parentElement.childNodes[1].childNodes[0].style.backgroundColor = "#33CD5F";
+        selected.parentElement.style.borderColor = "#33CD5F";
+        selected.parentElement.childNodes[1].childNodes[0].style.backgroundColor = "#33CD5F";
         console.log("Good job man, I'm proud of you!");
       }
 
       $scope.turn = true;
-      $scope.currentQuestion++;
-      $ionicLoading.hide();
-    }, 500);
+
 
 
   };
 
   $scope.next = function(){
-    console.log($scope.category);
-    if($scope.category.count > $scope.currentQuestion){
-      $state.go('app.category', { id: $scope.currentCategory, question: $scope.currentQuestion});
-    }else{
-      $state.go('app.finished');
-    }
+
+    $ionicLoading.show({
+        template: '<ion-spinner icon="ripple" class="spinner-positive" ></ion-spinner>',
+        animation: 'fade-in'
+      });
+
+    $timeout(function () {
+      $scope.currentQuestion++;
+      if($scope.category.count > $scope.currentQuestion){
+        $state.go('app.category', { id: $scope.currentCategory, question: $scope.currentQuestion});
+      }else{
+        $state.go('app.finished');
+      }
+      $ionicLoading.hide();
+    }, 500);
+
+  };
+
+  $scope.info = function(){
+    $ionicPopup.alert({
+      title: "დამატებითი ინფორმაცია",
+      template: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      okText: "დახურვა"
+    });
   };
 
 
